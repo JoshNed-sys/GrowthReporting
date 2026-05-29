630780781d69dba6d8d56120aa168a89b87b109e
JoshNed-sys <josh@nedhelps.com>
2026-05-29 08:51:25 -0500

    Update refresh.py

--- refresh.py
+++ refresh.py
@@ -111,6 +111,8 @@ for e in events:
     d = e.get("ActivityDate", "")
     if d:
         meetings_by_month[d[:7]] += 1
+        current_month = date.today().strftime("%Y-%m")
+meetings_by_month = {k: v for k, v in meetings_by_month.items() if k <= current_month}
 
 # Summaries
 total_pipeline  = sum(v["value"] for v in pipeline_by_stage.values())
