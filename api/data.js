7f0a81b09bd048894f85e7737d06599afb16d276
JoshNed-sys <josh@nedhelps.com>
2026-05-29 13:59:38 -0500

    Update data.js

--- api/data.js
+++ api/data.js
@@ -75,7 +75,7 @@ module.exports = async (req, res) => {
 
     // 2. Events this FY (past months only)
     const events = await soql(
-    `SELECT Id, ActivityDate, AccountId FROM Event WHERE ActivityDate >= ${FISCAL_YEAR}-01-01 AND ActivityDate <= ${today} AND AccountId IN (SELECT AccountId FROM Opportunity WHERE StageName IN (${stagesCsv}) AND AccountId != null) LIMIT 2000`
+    `SELECT Id, ActivityDate, AccountId FROM Event WHERE ActivityDate >= ${FISCAL_YEAR}-01-01 AND ActivityDate <= ${today} AND AccountId IN (SELECT AccountId FROM Opportunity WHERE StageName IN (${stagesCsv}) AND AccountId != null AND IsClosed = false) LIMIT 2000`
 );
 
     // 3. Closed won + lost this FY
